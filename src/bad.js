5d82520f6bd907df2e6efd9e4e12117e261b3587
Alex Lerma <alen.8719@gmail.com>
2025-09-04 16:01:37 -0600

    add bad.js file to trigger ESLint

--- src/bad.js
+++ src/bad.js
@@ -1,5 +1,1 @@
-function foo(x){
-  if (x == 1) console.log("loose eq"); // no curly, eqeqeq
-  let y = 2; // no-unused-vars
-}
-foo(1)
+function foo(x){ if (x == 1) console.log("loose eq"); let y = 2; } foo(1)
