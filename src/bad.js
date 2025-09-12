fa360188ef2a9f1fa2b1d2c6c7fe4f91ef1f1584
Alex Lerma <alen.8719@gmail.com>
2025-09-12 16:03:51 -0600

    fix: satisfy ESLint rules

--- src/bad.js
+++ src/bad.js
@@ -1,16 +1,9 @@
-function foo(x){ if(x==1)console.log("eqeq"); let y=2; }
-foo(1)
-// touch Thu Sep  4 16:44:22 CST 2025
-// touch Thu Sep  4 16:50:48 CST 2025
-// touch Thu Sep  4 16:56:17 CST 2025
-// touch Thu Sep  4 17:05:16 CST 2025
-// touch Thu Sep  4 17:08:38 CST 2025
-// touch Fri Sep  5 15:13:17 CST 2025
-// touch Fri Sep  5 15:18:40 CST 2025
-// touch Fri Sep  5 15:19:27 CST 2025
-// touch Fri Sep  5 15:23:50 CST 2025
-// scope test Fri Sep  5 16:31:40 CST 2025
-// scope test Fri Sep 12 15:01:17 CST 2025
-// scope test Fri Sep 12 15:07:22 CST 2025
-// scope test Fri Sep 12 15:09:28 CST 2025
-// checks api Fri Sep 12 15:15:11 CST 2025
+function foo(x) {
+  if (x === 1) {
+    console.log('eqeq');
+  }
+  // remove unused var or use it
+  const y = 2;
+  console.log(y);
+}
+foo(1);
