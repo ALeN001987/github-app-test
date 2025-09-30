54d6d15db54652395d66d6c1785b99be8464a41a
Alex Lerma <alen.8719@gmail.com>
2025-09-30 14:41:08 -0600

    again checking the ts file inside ts folder

--- src/ts/bug2.ts
+++ src/ts/bug2.ts
@@ -1,4 +1,4 @@
 export function add(a: number, b: number): number {
-    const wrong: string = a + b; // TS2322: number not assignable to string
+    const wrong: string = a + b; // TS2322
     return wrong as unknown as number;
   }
