38011f59be4dd37a0826afb92d51bdbaa23f1a13
Alex Lerma <alen.8719@gmail.com>
2025-09-30 14:14:17 -0600

    teste

--- src/ts/type-bug.ts
+++ src/ts/type-bug.ts
@@ -1,5 +1,4 @@
-/* TSC: assign string into number */
 export function add(a: number, b: number): number {
-    const wrong: string = a + b; // TS2322
+    const wrong: string = a + b; // TS2322: number not assignable to string
     return wrong as unknown as number;
   }
