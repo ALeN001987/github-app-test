a4f1951e27fd070139fb1a701d068a9f5c422ed8
Alex Lerma <alen.8719@gmail.com>
2025-09-30 14:08:11 -0600

    new queue file

---
+++ src/js/inline-bad.js
@@ -0,0 +1,5 @@
+/* ESLint: curly, semi, quotes */
+function isAdult(age){
+    if (age >= 18) return 'yes'
+    else return "no"
+  }
---
+++ src/mixed/needs-prettier2.ts
@@ -0,0 +1,2 @@
+/* Prettier: spacing, semicolons, quotes */
+export const things=[1,2, 3].map(n=>{return n*2})
