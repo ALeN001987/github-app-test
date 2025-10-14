4fae41debda6425fee560bc9b5e457a56cce0d5c
Alex Lerma <alen.8719@gmail.com>
2025-10-14 14:23:30 -0600

    definetlybrokentsfile

---
+++ src/definitely-broken.ts
@@ -0,0 +1,5 @@
+// Guaranteed TS errors
+export function brokenSum(a: number, b: number): number {
+    const s: string = a + b;   // TS2322: number is not assignable to string
+    return s;                  // TS2322: string is not assignable to number
+  }
